42a9f54a4712d9c62d48cefe80593c3c70aef463
JuliaMelle <toledopascuajuliamelle@gmail.com>
2023-08-25 22:26:28 +0800

    FRONTEND of LOGIN

---
+++ frontend/login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSS -->
+    <link rel="stylesheet" href="../css/global.css" />
+    <link rel="stylesheet" href="../css/login.css" />
+    <!-- BOOTSTAP 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+</head>
+
+<body class="body">
+    <div class="parent">
+
+        <div class=" mid_card card card_width ">
+            <div class="card-body card_width_body text_left">
+                <h3 class="card-title font_bold create_acc">WELCOME THOMASIANS</h3>
+                <!-- <p class="card-text">WELCOME TO UST</p> -->
+
+                <input class="form-control margin_forms font_bold" type="text" placeholder="EMAIL ADDRESS" aria-label="default input example">
+
+                <input class="form-control margin_forms font_bold" type="password" placeholder="PASSWORD" aria-label="default input example">
+
+            </div>
+            <div class="button_position">
+                <button type="button" class="btn btn-warning  button_size font_bold">LOGIN</button>
+                <button type="button" class="btn btn-warning  button_size font_bold register">REGISTER</button>
+
+            </div>
+        </div>
+
+    </div>
+
+    <script src="" async defer></script>
+</body>
+
+</html>
