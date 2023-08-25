3a8346296da8831c22b34ae0b30fcb9579ab77a4
JuliaMelle <toledopascuajuliamelle@gmail.com>
2023-08-26 01:59:42 +0800

    added required

---
+++ backend/register_data.php
@@ -0,0 +1,187 @@
+
+<?php
+session_start();
+if (isset($_POST['Submit'])) {
+
+    //1. Setup Database connection
+    require_once 'config.php';
+    $upload_dir = '../assets/user_identification/';
+    $upload_dir2 = '../assets/user_identification/';
+
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $number = $_POST['number'];
+    $program = $_POST['program'];
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+   
+    $password = md5($_POST['password']);
+
+    $imgName = $_FILES['image']['name'];
+    $imgTmp = $_FILES['image']['tmp_name'];
+    $imgSize = $_FILES['image']['size'];
+
+    $imgExt = strtolower(pathinfo($imgName, PATHINFO_EXTENSION));
+
+    $allowExt  = array('jpeg', 'jpg', 'png', 'gif');
+
+    $userPic = time() . '_' . rand(1000, 9999) . '.' . $imgExt;
+
+    // IMAGE SIZE
+    if (in_array($imgExt, $allowExt)) {
+
+        if ($imgSize < 5000000) {
+            move_uploaded_file($imgTmp, $upload_dir . $userPic);
+        } else {
+            $errorMsg = 'Image too large';
+        }
+    } else {
+        $errorMsg = 'Please select a valid image';
+    }
+
+    // 2nd image
+    $imgName2 = $_FILES['image_v2']['name'];
+    $imgTmp2 = $_FILES['image_v2']['tmp_name'];
+    $imgSize2 = $_FILES['image_v2']['size'];
+
+    $imgExt2 = strtolower(pathinfo($imgName2, PATHINFO_EXTENSION));
+
+    $allowExt  = array('jpeg', 'jpg', 'png', 'gif');
+
+    $userPic2 = time() . '_' . rand(1000, 9999) . '.' . $imgExt2;
+
+    // IMAGE SIZE
+    if (in_array($imgExt, $allowExt)) {
+
+        if ($imgSize2 < 5000000) {
+            move_uploaded_file($imgTmp2, $upload_dir2 . $userPic2);
+        } else {
+            $errorMsg = 'Image too large';
+        }
+    } else {
+        $errorMsg = 'Please select a valid image';
+    }
+
+    // if (empty($first_name) or empty($last_name) or empty($number)or empty($username) or empty($email) or empty($password)) {
+    //     $errorMsg = 'There is a missing input. Cannot complete registration. Please try again.';
+    //     header('Location: ../registration_form.php?authenticate=false');
+    // }
+
+    if (empty($first_name)) {
+        $errorMsg = 'There is a missing input. Cannot complete registration. Please try again.';
+        header('Location: ../registration_form.php?authenticate=false');
+    }
+  
+    if (empty($errorMsg)) {
+        // Perform further actions like saving data to a database or sending emails
+        // Redirect to a success page or display a success message
+        // Example: header("Location: success.php");
+        // Remember to exit or die after a redirect
+        exit;
+    }
+
+    $sql2 = "SELECT * FROM users WHERE username = '$username' OR email = '$email'";
+    $result2 = mysqli_query($conn, $sql2);
+    $count2 = mysqli_num_rows($result2);
+    $row2 = mysqli_fetch_assoc($result2);
+
+    if ($count2 > 0) {
+        header('Location: ../frontend/register.php?useremail=false');
+        } else {
+        $sql = "INSERT INTO `users`
+        (`first_name`, `last_name`, `number`, `program`, `email`, `username`, `password`, `profile`) 
+        VALUES (
+            '" . $first_name . "',
+            '" . $last_name . "',
+            '" . $number . "',
+            '" . $program . "',
+            '" . $email . "',
+            '" . $username . "',
+            '" . $password . "',
+            '" . $userPic . "'
+            )";
+
+        //3. Execute SQL
+        if (mysqli_query($conn, $sql)) {
+            $_SESSION['loggedin'] = TRUE;
+            $_SESSION['username'] = $username;
+            $_SESSION['password'] = $password;
+            header('Location:../frontend/index.php');        
+        } else {
+            header('Location: ../registration_form?authenticate=false');
+        }
+
+        mysqli_close($conn);
+    }
+
+
+    // $sql2 = "SELECT * FROM users";
+    // if ($result = $conn->query($sql2)) {
+    //     if ($result->num_rows > 0) {
+    //         while ($row = $result->fetch_array()) {
+    //             if (($username == $row['username'])) {
+    //                 header('Location: ../registration_form.php?username=false');
+    //             } 
+    //             if (($email == $row['email'])) {
+    //                 header('Location: ../registration_form.php?email=false');
+    //             }
+    //             if (($username == $row['username']) and ($email == $row['email'])) {
+    //                 header('Location: ../registration_form.php?useremail=false');
+    //             }
+    //         }
+    //     }
+    // }
+
+
+
+    $sql3 = "SELECT * FROM users";
+    if ($result = $conn->query($sql3)) {
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_array()) {
+                if (empty($first_name) or empty($username) or empty($email) or empty($password) or empty($address)) {
+                    $errorMsg = 'There is a missing input. Cannot complete registration. Please try again.';
+                    header('Location: ../registration_form.php?authenticate=false');
+                }
+                if (($username == $row['username'])) {
+                    header('Location: ../registration_form.php?username=false');
+                }
+                if (($email == $row['email'])) {
+                    header('Location: ../registration_form.php?email=false');
+                }
+                if (($username == $row['username']) and ($email == $row['email'])) {
+  header('Location: ../frontend/register.php?useremail=false');                
+}
+                if (($username !== $row['username'] or $email !== $row['email']) and ($username == $row['username']) and ($email == $row['email'])) {
+                    $sql = "INSERT INTO `users`
+                (`first_name`, `username`, `email`, `password`, `address`, `profile_img`, `valid_id_img`) 
+                VALUES (
+                    '" . $first_name . "',
+                    '" . $username . "',
+                    '" . $email . "',
+                    '" . $password . "',
+                    '" . $address . "',
+                    '" . $userPic . "',
+                    '" . $userPic2 . "'
+                    )";
+
+                    //3. Execute SQL
+                    if (mysqli_query($conn, $sql)) {
+                        $_SESSION['loggedin'] = TRUE;
+                        $_SESSION['username'] = $username;
+                        $_SESSION['password'] = $password;
+
+                        // header('Location:authenticate2.php');
+                        header('Location:../frontend/index.php');
+
+                    } else {
+                        mysqli_error($conn);
+                        header('Location: ../registration_form?authenticate=false');
+                    }
+
+                    mysqli_close($conn);
+                }
+            }
+        }
+    }
+}
+?>
--- frontend/register.php
+++ frontend/register.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
+    <title>Thomasian OrgLink</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSS -->
@@ -22,29 +22,76 @@
 
 <body class="body">
     <div class="parent">
+    <?php
+        if (isset($_GET['authenticate'])) { //check if authenticate key exists in URL
+          if ($_GET['authenticate'] == "false") {
+        ?>
+            <br>
+            <div class="alert alert-warning d-flex align-items-center capsule" role="alert">
+                    <div>
+                    Invalid</strong> Please try again
+                    </div>
+                </div>
+        <?php
+          }
+        }
+        ?>
+          <?php
+        if (isset($_GET['useremail'])) { //check if authenticate key exists in URL
+          if ($_GET['useremail'] == "false") {
+        ?>
+   <br>
+            <div class="alert alert-warning d-flex align-items-center capsule" role="alert">
+                    <div>
+                    Invalid<strong> username or email. Already existing.</strong> Please try again.
+                    </div>
+                </div>
 
+<?php
+          }
+        }
+        ?>
         <div class=" mid_card card card_width ">
             <div class="card-body card_width_body text_left">
                 <h3 class="card-title font_bold create_acc">CREATE ACCOUNT</h3>
                 <!-- <p class="card-text">WELCOME TO UST</p> -->
-                <input class="form-control margin_forms font_bold" type="text" placeholder="FIRST NAME" aria-label="default input example">
-                <input class="form-control margin_forms font_bold" type="text" placeholder="LAST NAME" aria-label="default input example">
-                <input class="form-control margin_forms font_bold" type="text" placeholder="+63" aria-label="default input example">
-                <input class="form-control margin_forms font_bold" type="text" placeholder="DEGREE PROGRAM" aria-label="default input example">
-                <input class="form-control margin_forms font_bold" type="text" placeholder="EMAIL ADDRESS" aria-label="default input example">
-                <input class="form-control margin_forms font_bold" type="text" placeholder="USERNAME" aria-label="default input example">
-                <input class="form-control margin_forms font_bold" type="password" placeholder="PASSWORD" aria-label="default input example">
-                <div class="mb-3">
-                    <label for="formFile" class="form-label text_left font_bold">PROFILE PICTURE</label>
-                    <input class="form-control" type="file" id="formFile">
-                </div>
+                <form class="form-flex column" id="regForm" action="../backend/register_data.php" method="post" enctype="multipart/form-data">
+                    <input class="form-control margin_forms font_bold" type="text" placeholder="FIRST NAME" aria-label="default input example" name="first_name" required>
+                    <input class="form-control margin_forms font_bold" type="text" placeholder="LAST NAME" aria-label="default input example" name="last_name" required>
+                    <input class="form-control margin_forms font_bold" type="text" placeholder="+63" aria-label="default input example" name="number" required> 
+                    <!-- <input class="form-control margin_forms font_bold" type="text" placeholder="DEGREE PROGRAM" aria-label="default input example" name="program"> -->
+                    <select class="form-select font_bold gray margin_forms" placeholder="DEGREE PROGRAM" name="program" >
+                        <option selected>DEGREE PROGRAM </option>
+                        <option value="FACULTY OF ARTS AND LETTERS" name="program">FACULTY OF ARTS AND LETTERS</option>
+                        <option value="AMV COLLEGE OF ACCOUNTANCY" name="program">AMV COLLEGE OF ACCOUNTANCY</option>
+                        <option value="COLLEGE OF ARCHITECTURE" name="program">COLLEGE OF ARCHITECTURE</option>
+                        <option value="COLLEGE OF COMMERCE AND BUSINESS ADMINISTRATION" name="program">COLLEGE OF COMMERCEAND BUSINESS ADMINISTRATION</option>
+                        <option value="COLLEGE OF FINE ARTS AND DESIGN" name="program">COLLEGE OF FINE ARTS AND DESIGN</option>
+
+                        <option value="COLLEGE OF EDUCATION" name="program">COLLEGE OF EDUCATION</option>
+                        <option value="COLLEGE OF TOURISM AND HOSPITALITY MANAGEMENT" name="program">COLLEGE OF TOURISM AND HOSPITALITY MANAGEMENT</option>
+                        <option value="COLLEGE OF INFORMATION AND COMPUTING SCIENCE" name="program">COLLEGE OF INFORMATION AND COMPUTING SCIENCE</option>
+                        <option value="FACULTY OF ENGINEERING" name="program">FACULTY OF ENGINEERING</option>
+                        <option value="FACULTY OF PHARMACY" name="program">FACULTY OF PHARMACY</option>
+                        <option value="FACULTY OF MEDICINE AND SURGERY" name="program">FACULTY OF MEDICINE AND SURGERY</option>
+                        <option value="COLLEGE OF NURSING" name="program">COLLEGE OF NURSING</option>
+
+                    </select>
+                    <input class="form-control margin_forms font_bold" type="text" placeholder="EMAIL ADDRESS" aria-label="default input example" name="email" required>
+                    <input class="form-control margin_forms font_bold" type="text" placeholder="USERNAME" aria-label="default input example" name="username" required>
+                    <input class="form-control margin_forms font_bold" type="password" placeholder="PASSWORD" aria-label="default input example" name="password" required>
+                    <div class="mb-3">
+                        <label for="formFile" class="form-label text_left font_bold">PROFILE PICTURE</label>
+                        <input class="form-control" type="file" id="formFile" name="image"></input>
+                    </div>
             </div>
             <div class="button_position">
-                <button type="button" class="btn btn-warning  button_size font_bold">LOGIN</button>
-                <button type="button" class="btn btn-warning  button_size font_bold register">REGISTER</button>
+                <a href="login.php" button type="button" class="btn btn-dark  button_size font_bold">LOGIN</button></a>
+                <button type="submit" name="Submit" class="btn btn-warning  button_size font_bold register">REGISTER</button>
 
             </div>
         </div>
+        </form>
 
     </div>
 
