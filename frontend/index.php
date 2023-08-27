ab9375e99ae963dc3cf1b243ddde9f6e565a243b
Nyraea <82209986+Nyraea@users.noreply.github.com>
2023-08-27 16:47:57 +0800

    view_orgs page added & index anchored

--- frontend/index.php
+++ frontend/index.php
@@ -17,8 +17,6 @@
   <!-- BOOTSTAP 5 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  <!-- FAVICON -->
-  <link rel="icon" type="image/png" sizes="32x32" href="../assets/logoicon.png">
 </head>
 
 <body style="height: 100vh">
@@ -82,9 +80,8 @@
   <div class="parent">
     <div class="mid">
       <div class="button_position">
-        <button type="button" class="btn btn-dark  button_mid"> GET STARTED </button>
-
-        <button type="button" class="btn btn-dark  button_mid button_view"> VIEW ORGS </button>
+        <button role="button" class="btn btn-dark  button_mid"> GET STARTED </button>
+       <a href = " view.php" class="btn btn-dark  button_mid button_view" formaction="view.php"> VIEW ORGS </a>
       </div>
       <hr class="new4" />
       <h1 class="head_text">FEATURED ORGANIZATIONS</h1>
