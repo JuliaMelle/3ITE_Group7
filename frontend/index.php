44fdd78e8c1b84121fa34e9f41594f620b4d7fda
aliahestbn <aliah.esteban.cics@ust.edu.ph>
2023-08-28 23:13:23 +0800

    added favicon

--- frontend/index.php
+++ frontend/index.php
@@ -17,6 +17,8 @@
   <!-- BOOTSTAP 5 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  <!-- FAVICON -->
+  <link rel="icon" type="image/png" sizes="32x32" href="../assets/logoicon.png">
 </head>
 
 <body style="height: 100vh">
@@ -33,7 +35,7 @@
       <div class="carousel-item active">
         <img src="../assets/2.png" class="d-block w-100 filter" alt="..." />
         <div class="carousel-caption d-none d-md-block caption_carousel">
-          <b class="welcome">WELCOME TO THOMASIANS</b>
+          <b class="welcome">WELCOME TO THOMASIANS!</b>
 
           <h1 class="Head_carousel_text">
             To form competent and compassionate persons committed to serve the
